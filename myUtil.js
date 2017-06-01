46085e9b7d41ed8dc8968f28c1e43948d34ae0bb
shubhang <shubhang@channelbridge.in>
2017-06-01 22:19:02 +0530

    add util

---
+++ myUtil.js
@@ -0,0 +1,16 @@
+var myMap = function(arr, fn) {
+  var res = []
+  arr.forEach(function(v,i,a) {
+      res.push(fn(v,i,a))
+    })
+    return res
+}
+
+var myFilter = function(arr, fn) {
+  var res = []
+  arr.forEach(function(v,i,a) {
+    if(fn(v,i,a))
+      res.push(v)
+  })
+  return res
+}
