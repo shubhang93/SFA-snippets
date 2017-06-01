a2047f131bc32efc6f6a8cbde8cd874c4269c89d
shubhang93 <shubhang.sabbath@gmail.com>
2017-06-01 17:08:04 +0530

    Update groupByProp.js

--- groupByProp.js
+++ groupByProp.js
@@ -1,3 +1,3 @@
 var grouByProp = (prop) => (list) => list.reduce((ax, curr, idx, list) => Object.assign({}, ax, {
- [curr.id]: list.filter(e => e.id === curr.id)
+    [curr.id]: list.filter(e => e[prop] === curr[prop])
 }), {})
