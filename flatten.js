9091c51dfd940ad7e979a3620e9cb4549bc99cf6
shubhang <shubhang@channelbridge.in>
2017-06-10 19:38:31 +0530

    arrayFlatten

---
+++ flatten.js
@@ -0,0 +1,8 @@
+var flattenFn = function(list, oldAx = []) {
+  return list.reduce((ax,curr) => {
+    if(curr instanceof Array) {
+      return flattenFn(curr, ax)
+    }
+    return [...ax, curr]
+  },oldAx)
+}
